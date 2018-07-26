1c589192d6ad942c4cef8adb2009a4d82d493573
Gurpreet Singh <30008846@student.toiohomai.ac.nz>
2018-07-26 14:28:08 +1200

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("hello world");
+                
+
+            // or it can be done on a single line
+           // Console.WriteLine("Hello world");
+
+            // We use this console.readline() to stop the application automatically closing.
+            Console.ReadLine();
+        }
+        
+          
+
+    }
+}
